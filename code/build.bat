0e187af33b7d98f1b3b1fadeaa16bf4fc3801de2
Michael Eggers <eggers@hm.edu>
2018-11-23 20:37:46 +0100

    dont linking the kernel against glu and opengl. Not needed there!

--- code/build.bat
+++ code/build.bat
@@ -7,7 +7,7 @@ REM render device implementation for windows
 cl /Zi /FC /MTd ..\code\ogl_render.cpp ..\code\helper.cpp opengl32.lib glu32.lib gdi32.lib user32.lib /link /DLL /DEF:..\code\renderer_gl.def /OUT:win32_opengl.dll
 
 REM kernel
-cl /Zi /FC ..\code\win32_platform.cpp ..\code\helper.cpp user32.lib gdi32.lib opengl32.lib dwmapi.lib glu32.lib
+cl /Zi /FC ..\code\win32_platform.cpp ..\code\helper.cpp user32.lib gdi32.lib dwmapi.lib
 
 REM cl /Zi /FC ..\code\foo.cpp
 
