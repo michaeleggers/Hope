89fb3919bd8ee2eb3b766b3a2f2d0fa9d799de65
Eggers <hm-eggers@hm.edu>
2019-06-04 19:23:55 +0200

    passing DWORD to ReadFile for #bytes read, Because passing LPDWORD actually points to nothing. That caused the ReadFile function to fail everytime on Windows 7. It should have failed on any system, though, because it was just plain wrong.

--- code/win32_platform.cpp
+++ code/win32_platform.cpp
@@ -68,17 +68,17 @@ PLATFORM_READ_TEXT_FILE(win32ReadTextFile)
     DWORD filesize = GetFileSize(fileHandle, NULL);
     
     // VirtualAlloc actually allocates a whole page (buffer will be 4k)
-    char* buffer = (char*)VirtualAlloc(
+    char *buffer = (char*)VirtualAlloc(
         NULL,
         filesize * sizeof(char),
-        MEM_COMMIT,
+        MEM_RESERVE|MEM_COMMIT,
         PAGE_READWRITE
         );
     
     _OVERLAPPED ov = {0};
-    LPDWORD numBytesRead = 0;
+    DWORD numBytesRead = 0;
     DWORD error;
-    if (ReadFile(fileHandle, buffer, filesize, numBytesRead, NULL) == 0)
+    if (ReadFile(fileHandle, buffer, filesize, &numBytesRead, NULL) == 0)
     {
         error = GetLastError();
         char errorMsgBuf[256];
@@ -387,6 +387,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine,
     
     game_init(&platformAPI, &re);
     OutputDebugStringA("game init done\n");
+    
+    XInputInit();
     
     // XBox Controller state
     XBoxControllerState controllerState = {};
